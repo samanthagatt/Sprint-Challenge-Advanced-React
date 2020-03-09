cf8f0444e7ae27439af29b40f2b42a35e50c8953
Samantha Gatt <smfgatt@gmail.com>
2020-03-09 14:34:02 -0400

    Fetches data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    women: []
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/players")
+      .then(res => {
+        this.setState({women: res.data});
+      }).catch(err => {
+        alert(`An error occurred while trying to fetch data:\n ${err}`);
+      })
+  }
+
   render() {
     return (
       <div className="App">
